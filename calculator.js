3212fd034a613912e792d75f155a557787120a4b
Syed Humza <105381562+syedhumzasalman@users.noreply.github.com>
2025-01-08 09:01:15 +0500

    update function

--- calculator.js
+++ calculator.js
@@ -1,39 +1,19 @@
-
 let input = document.getElementById("input");
 
-
-function display(num){
-    input.value += num;
+function display(num) {
+  input.value += num;
 }
 
-
-function equal(){
-   let finalNumber = input.value;
-   let finalResult = eval(finalNumber);
-   input.value = finalResult;
+function equal() {
+  input.value = eval(input.value);
 }
 
-
-function clr(){
-    input.value = "";
+function clr() {
+  input.value = "";
 }
 
-function del(){
-    input.value = input.value.slice(0,-1)
+function del() {
+  input.value = input.value.slice(0, -1);
 }
 
-
-function histroy(){
-    
-}
-
-
-
-
-
-
-
-
-
-
-
+function histroy() {}
